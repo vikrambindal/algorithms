1a244550ad44cfd4ff24186b2e2577bef0ce6d69
Vikram Bindal <vikram.bindal@kurtosys.com>
2018-07-19 17:55:42 +0200

    Added Bubble and Selection Sort

---
+++ src/vik/demo/sort/SelectionSort.java
@@ -0,0 +1,48 @@
+package vik.demo.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		SelectionSort ss = new SelectionSort();
+		
+		int[] data = {5, 9, 3, 1, 2, 8, 4, 7, 6, 0};  
+		
+		System.out.println("Before: ");
+		ss.printData(data);
+		
+		ss.sort(data);
+		
+		System.out.println("\nAfter: ");
+		ss.printData(data);
+	}
+	
+	private void printData(int[] data) {
+		Arrays.stream(data).forEach(System.out::print);
+	}
+	
+	public void sort(int[] data) {
+		int iterations = 0;
+		int swaps = 0;
+		
+		for ( int i = 0 ; i < data.length ; i++ ) {
+			int minIdx = i;
+			for ( int j = i+1; j < data.length ; j++) {
+				
+				iterations++;
+				
+				if (data[j] < data[minIdx]) {
+					minIdx = j;
+				}
+			}
+			
+			int temp = data[minIdx];
+			data[minIdx] = data[i];
+			data[i] =  temp;
+			swaps++;
+		}
+		
+		System.out.println(String.format("\n[Iterations=%s Swaps=%s]", iterations, swaps));
+	}
+}
